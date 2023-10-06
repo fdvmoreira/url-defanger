27c7479c458f2e6703b9d1c20904fef41866e105
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-10-06 18:44:40 +0100

    apply basic style to the form component

--- src/components/form.rs
+++ src/components/form.rs
@@ -10,9 +10,9 @@ pub struct Props {
 #[function_component]
 pub fn Form(props: &Props) -> Html {
     html! {
-         <form onsubmit={&props.onsubmit}>
-             <input type="text" name="url" class="text" ref={&props.input_ref} placeholder="Insert URL here" />
-             <input type="submit" value="Defange URL" />
+         <form class={classes!(String::from("flex justify-center items-center m-4"))} onsubmit={&props.onsubmit}>
+             <input type="text" name="url" class={classes!(String::from("text-xl border p-4 m-2 xs:w-full md:w-96"))} ref={&props.input_ref} placeholder="ex: https://defange.url" />
+             <input type="submit" value="Defange" class={classes!(String::from("uppercase bg-indigo-600 hover:bg-indigo-800 font-bold text-slate-50 p-4 px-8 m-2"))} />
          </form>
     }
 }
