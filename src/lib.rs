f022f64c6826a1ab680d4a79bacb88efd8bb7988
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-10-08 22:17:48 +0100

    update: write unit test and implement trivial function to check if url is valid

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,34 @@ pub fn is_char_doubled(str: &str, char: char, succession_len: Option<u32>) -> bo
     // str.find(String::from_iter(pattern).as_str()).is_some()
     str.contains(String::from_iter(pattern).as_str())
 }
+
+pub fn is_valid_url(url: &str) -> bool {
+    // http://www.example.com:80/path/to/myfile.html?key1=value1&key2=value2#SomewhereInTheDocument
+    // Url contains:
+    // scheme     => http
+    // domain     => www.example.com
+    // port       => 80
+    // path       => /path/to/myfile.html
+    // parameters => ?key1=value1&key2=value2
+    // anchors    => #SomewhereInTheDocument
+    //
+
+    let url = url.trim();
+    let mut chars = url.chars();
+    let start = &chars.next().expect("Could Not Get the 1st Char");
+    let end = &chars.last().expect("Could Not get the last Char");
+
+    // TODO: implement better validation rules
+
+    if !url.is_ascii()
+        || !start.is_ascii_alphabetic()
+        || end.eq(".".chars().next().as_ref().unwrap())
+    {
+        return false;
+    }
+
+    true
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,6 +82,27 @@ mod tests {
         );
         assert_eq!(defange_url("url"), Some("url".to_string()));
         assert_eq!(defange_url(""), None)
+    }
+
+    #[test]
+    fn test_validate_url() {
+        let urls = [
+            "example.com",
+            "http://awesome.things",
+            ".http://example.net",
+            "http://example.com.",
+            "https://mythiongs.lol",
+            "http::/hows.lod",
+            "http:///kdsds..ijd",
+        ];
+
+        assert_eq!(is_valid_url(&urls[0]), true);
+        assert_eq!(is_valid_url(&urls[1]), true);
+        assert_eq!(is_valid_url(&urls[2]), false);
+        assert_eq!(is_valid_url(&urls[3]), false);
+        assert_eq!(is_valid_url(&urls[4]), true);
+        assert_eq!(is_valid_url(&urls[5]), false);
+        assert_eq!(is_valid_url(&urls[6]), false);
     }
 
     #[test]
