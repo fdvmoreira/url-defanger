5346c470314d604a19459d04edbb1b34d68435cb
Flavio Moreira <mr.flaviomoreira@gmail.com>
2023-10-09 22:16:47 +0100

    update: remove the purge field as it is redundant in the new tailwindcss version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    mode: "all",
-    content: [
-      "./index.html",
-      "./src/**/*.html",
-      "./src/**/*.rs",
-      "./src/**/*.css",
-    ],
-  },
   content: ["./*.{html,js,css,rs}", "./src/**/*.{rs,html,css,js}"],
   theme: {
     extend: {},
